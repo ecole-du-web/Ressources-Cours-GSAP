be064332f41d881ebbff69d0cd1b515a43f57f92
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2021-06-11 17:02:12 +0200

    Update app.js

--- 3. Projets/2.ProjetsTerminés/5. Mutiple Scroll/app.js
+++ 3. Projets/2.ProjetsTerminés/5. Mutiple Scroll/app.js
@@ -1,18 +1,5 @@
-const backgroundImg = document.querySelector(".home-mountains");
 const longMountain = document.querySelector(".home-mountains img:nth-child(1)");
-const rightRock = document.querySelector(".home-mountains img:nth-child(2)");
-const bottomRock = document.querySelector(".home-mountains img:nth-child(3)");
-const pic = document.querySelector(".home-mountains img:nth-child(4)");
-
 const title = document.querySelector('h1');
-
-gsap.to(backgroundImg, {
-  scrollTrigger: {
-    trigger: 'home-mountains',
-    scrub: true,
-    pin: true,
-  }
-});
 
 
 gsap.to(longMountain, {
